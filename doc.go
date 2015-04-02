1088be97b03d91b91e766e1681cb9739fc79f43d
HeroicYang <me@heroicyang.com>
2015-04-02 23:57:47 +0800

    update doc

--- doc.go
+++ doc.go
@@ -1,6 +1,12 @@
 // 企业微信号 API 的 Golang 版本
+//
 // github.com/heroicyang/wechat-qy
 //
 // @author  Heroic Yang <me@heroicyang.com>
+//
 // @license MIT
 package wechat
+
+import (
+	_ "github.com/heroicyang/wechat-qy/suite"
+)
